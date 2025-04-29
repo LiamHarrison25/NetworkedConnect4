6bf613c1d36e57316621dd21799a7e0c10699f5d
Liam Harrison <liam.harrison@mymail.champlain.edu>
2025-04-29 12:01:34 -0400

    World Packet

--- main.cpp
+++ main.cpp
@@ -6,11 +6,57 @@
 #include "game.h"
 #include "serialization.h"
 
-
+void WriteString(const std::string& data, std::stringstream& stream);
 
 std::string create_world_packet(const std::vector<std::unique_ptr<GameObject>>& objects) {
     // TODO: Implement this
-    return "";
+
+    std::stringstream stream;
+
+    // Get the number of objects in the world
+
+    uint32_t objectCount = objects.size();
+
+    std::string objectCountS = std::to_string(objectCount);
+
+    WriteString(objectCountS, stream);
+
+    // Loop through all the objects
+
+    int i;
+    for (i = 0; i < objectCount; i++)
+    {
+        // The integer type of the object (4 bytes)
+        uint32_t objectType = objects[i].get()->type;
+
+        std::string objectTypeS = std::to_string(objectType);
+
+        WriteString(objectTypeS, stream);
+
+        // The objects serialized data
+
+        std::string objectData = "";
+
+        std::ostringstream dataStream;
+        objects[i].get()->Serialize(dataStream);
+
+        objectData = dataStream.str();
+
+        WriteString(objectData, stream);
+    }
+
+    return stream.str();
+}
+
+void WriteString(const std::string& data, std::stringstream& stream)
+{
+    //Writing the string to binary
+    int i;
+    for (i = 0; i < data.size(); i++)
+    {
+        const char* messageChar = &data[i];
+        stream.write(messageChar, sizeof(char));
+    }
 }
 
 void deserialize_packet(const std::string& data) {
@@ -24,7 +70,18 @@ void GameObject::Deserialize(std::istream &is) {
 
     // TODO: Implement this (leave code before this line intact)
 
+    //position
+    stream_read(is, position);
 
+    //rotation
+    stream_read(is, rotation);
+
+     //collision_radius
+    stream_read(is, collision_radius);
+
+    //type
+    //stream_read(is, type);
+    
 }
 
 void GameObject::Serialize(std::ostream &os) {
@@ -33,17 +90,18 @@ void GameObject::Serialize(std::ostream &os) {
     }
 
     // TODO: Implement this (leave code before this line intact)
-    
-    //Serialize:
 
     //position
     stream_write(os, position);
     //rotation
     stream_write(os, rotation);
-    //type
-    stream_write(os, type);
+
     //collision_radius
     stream_write(os, collision_radius);
+
+    //type
+    //stream_write(os, type);
+    
 
 }
 
@@ -52,41 +110,101 @@ void Bullet::Serialize(std::ostream &os) {
 
     GameObject::Serialize(os);
 
-    //velocity
-    stream_write(os, velocity);
-
     //team
     stream_write(os, team);
+
+    //velocity
+    stream_write(os, velocity);    
 
 }
 
 void Bullet::Deserialize(std::istream &is) {
     // TODO: Implement this
+
+    GameObject::Deserialize(is);
+
+    stream_read(is, team);
+
+    stream_read(is, velocity);
+
 }
 
 void Player::Serialize(std::ostream &os) {
     // TODO: Implement this
+
+    GameObject::Serialize(os);
+
+    // team
+    stream_write(os, team);
+
+    // health
+    stream_write(os, health);
+
+    // spawn_point
+    stream_write(os, spawn_point);
+
+    // velocity
+    stream_write(os, velocity);
+
+    // frame_thrust
+    //stream_write(os, frame_thrust);
+
+    // is_net_controlled
+    //stream_write(os, is_net_controlled);
 }
 
 void Player::Deserialize(std::istream &is) {
     // TODO: Implement this
+
+    //stream_read(is, is_net_controlled);
+
+    //stream_read(is, frame_thrust);
+
+    GameObject::Deserialize(is);
+
+    stream_read(is, team);
+
+    stream_read(is, health);
+
+    stream_read(is, spawn_point);
+
+    stream_read(is, velocity);
+
 }
 
 void Explosion::Serialize(std::ostream &os) {
     // TODO: Implement this
+
+    GameObject::Serialize(os);
+
+    // alive_time
+    stream_write(os, alive_time);
+
+    // color
+    stream_write(os, color);
+
 }
 
 void Explosion::Deserialize(std::istream &is) {
     // TODO: Implement this
+
+    GameObject::Deserialize(is);
+
+    //alive_time
+    stream_read(is, alive_time);
+
+    // color
+    stream_read(is, color);
 }
 
 int run_all_tests();
 void print_test_solution();
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
+    //if (argc > 1)
+    //{
         return run_all_tests();
-    }
+    //}
 
     InitWindow(640, 480, "Object State Replication");
     SetTargetFPS(60);
